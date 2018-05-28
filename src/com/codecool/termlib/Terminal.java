f18090852b96d342cf870b88b01e31278f7e20eb
attilaKincsei <akincsei@gmail.com>
2018-05-28 18:24:23 +0200

    resetStyle and clearScreen implemented

--- src/com/codecool/termlib/Terminal.java
+++ src/com/codecool/termlib/Terminal.java
@@ -30,6 +30,8 @@ public class Terminal {
      * (i.e.: underlined, dim, bright) to the terminal defaults.
      */
     public void resetStyle() {
+        String resetAll = String.format("0%s", STYLE);
+        command(resetAll);
     }
 
     /**
@@ -38,6 +40,8 @@ public class Terminal {
      * Might reset cursor position.
      */
     public void clearScreen() {
+        String clearScr = CLEAR + CONTROL_CODE + MOVE;  // "\033c" for really resetting the terminal
+        command(clearScr);
     }
 
     /**
@@ -61,7 +65,7 @@ public class Terminal {
      */
     public void setColor(Color color) {
     }
-
+    
     /**
      * Set the background printing color.
      *
@@ -70,7 +74,7 @@ public class Terminal {
      * @param color The background color to set.
      */
     public void setBgColor(Color color) {
-	String setbgcolor = "42m";
+	String setbgcolor = "31m";
 	command(setbgcolor);
     }
 
