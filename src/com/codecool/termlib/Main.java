062a80dca29de48527ef61a5a30129bfec751f58
Unknown <regoczi.tamas01@gmail.com>
2018-05-29 19:44:46 +0200

    word class intialized, plus some tinkering

--- src/com/codecool/termlib/Main.java
+++ src/com/codecool/termlib/Main.java
@@ -1,21 +1,51 @@
 import com.codecool.termlib.Terminal;
 import com.codecool.termlib.Color;
+import com.codecool.termlib.Word;
+import java.lang.*;
+import java.util.Scanner;
+import java.io.IOException;
 
 public class Main{
 	
 	public static void main(String[] args){
 		Terminal newTerminal = new Terminal();
-		newTerminal.setBgColor(Color.RED);
-
-		newTerminal.setColor(Color.BLUE);
-		System.out.println("testline");
 		
 
-		System.out.println("Hello World");
-		newTerminal.resetStyle();
-		System.out.println("Hello World");
-		newTerminal.clearScreen();
+		newTerminal.setColor(Color.BLUE);
+		
+		String inputchar = "";
+		
+		newTerminal.moveTo(24,0);
+		newTerminal.setup();
+		int starttime = (int)System.currentTimeMillis();
+		int count = 0;
+		
+		
+		while(true){
+			newTerminal.saveCursor();
+			int currentTime = (int)System.currentTimeMillis();
+			if(currentTime - starttime > 1000){
+				count++;
+				newTerminal.clearScreen();
+				newTerminal.setWord("szó",0+count,24);
+				newTerminal.moveTo(24,0);
+				starttime = (int)System.currentTimeMillis();
+			}
+			
+			newTerminal.restoreCursor();					
+		        
+			
+			
+			
+		
+		
+		}
+		
+		}
+		
+		
+
  
-	}
+	
 
 }
