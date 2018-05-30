9486d190c3279c150e2f7d6fa076df9ea5a658fc
Papp Gergely <pappgergo096@gmail.com>
2018-05-30 08:58:45 +0200

    Game included in Main

--- src/com/codecool/termlib/Main.java
+++ src/com/codecool/termlib/Main.java
@@ -1,20 +1,30 @@
 import com.codecool.termlib.Terminal;
 import com.codecool.termlib.Color;
+import com.codecool.termlib.Game;
 
 public class Main{
 	
 	public static void main(String[] args){
-		Terminal newTerminal = new Terminal();
-		newTerminal.setBgColor(Color.RED);
+		// Terminal newTerminal = new Terminal();
+		// newTerminal.setBgColor(Color.RED);
 
-		newTerminal.setColor(Color.BLUE);
-		System.out.println("testline");
+		// newTerminal.setColor(Color.BLUE);
+		// System.out.println("testline");
 		
 
-		System.out.println("Hello World");
-		newTerminal.resetStyle();
-		System.out.println("Hello World");
-		newTerminal.clearScreen();
+		// System.out.println("Hello World");
+		// newTerminal.resetStyle();
+		// System.out.println("Hello World");
+		// newTerminal.clearScreen();
+
+		Game game = new Game();
+		String[] words = game.dictonary;
+
+		for(int i = 0; i < words.length; i++) {
+			System.out.println(words[i]);
+
+		}
+
  
 	}
 
